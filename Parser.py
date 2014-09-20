36856ee744e7d4775e7f2f5e40d41bcc91077a20
Thomas Liu <thomasliu02@gmail.com>
2014-09-20 10:28:02 -0500

    Fixed replace_indices to use new Theme

--- Parser.py
+++ Parser.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 from nltk.tokenize.punkt import PunktWordTokenizer
-import Keyword
+import Theme
 
 class Parser:
-    def __init__(self, text):
+    def __init__(self, text, old_theme_words, new_theme_words):
         """Initialize parser"""
-        self._corpus = text
-        pass
+        self._old_theme = Theme.Theme(text, old_theme_words)
+        self._new_theme = Theme.Theme(text, new_theme_words)
 
     def replace_indices(self, indices, text):
         """Replace the indices of the tokenized text"""
@@ -16,8 +16,10 @@ class Parser:
         result = [token.replace(filter(str.isalnum, token), )]
         
         for index in indices:
+            pos = "NN"
             text_tokens[index] = text_tokens[index].replace(filter(str.isalnum, text_tokens[index]),
-                                                            Keyword.Keyword("N", self._corpus, filter(str.isalnum, text_tokens[index])).word_rank(0))
+                                                            self._old_theme.replace(self._new_theme, filter(str.isalnum, text_tokens[index]), pos),
+                                                            )
 
         return result
 
