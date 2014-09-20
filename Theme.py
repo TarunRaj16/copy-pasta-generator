6ed9338ebddf9b2f6e61787dc8ce098947250f6b
Thomas Liu <thomasliu02@gmail.com>
2014-09-20 10:06:12 -0500

    Reworked access function to go outward from the target distance

--- Theme.py
+++ Theme.py
@@ -9,27 +9,28 @@ class Theme():
 
     def word_rank(self, num):
         return self._sim_words[num]
+    
     def distance(self, replacee):
         """Calculate the distance between a word and the theme"""
-        for similar_word_list in self.similar_word_lists:
+        for similar_word_list in self._similar_word_lists:
             try:
                 return similar_word_list.index(replacee)
             except:
                 continue
             
         return -1
+
+    def find_pos(self, replacee):
+        return "NN"
     
-    def access(self, num, replacee)
-        pos_type = replacee.get_pos                 """will look into later"""
-        if self._sim_words1[num].is_correct_pos:     """will look into later"""
-            return self._sim_words1[num]
-        for i in range(num, num+20):
-            if self._sim_words1[i].is_correct_pos:
-                return self._sim_words1[i]
-        for i in range(num-20, num-1):
-            if self._sim_words1[i].is_correct_pos:
-                return self._sim_words1[i]
-            
+    def access(self, distance, fuzziness, new_theme, replacee, replacee_pos)
+        """Fuzzily access the word at a certain distance away"""
+        
+        for similar_word_list in new_theme.similar_word_lists:
+            for ii in [round(jj * (-0.5 if jj%2 == 0 else 0.5)) for jj in range(0,fuzziness * 2) ]:
+                if find_pos(similar_word_list[ii]) == replacee_pos:
+                    return similar_word_list[ii]
+                
 """
 def main():
     textG = nltk.Text(word.lower() for word in nltk.corpus.brown.words())
