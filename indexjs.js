6f06fc222ebe3d322c6fceb3785190f011858bc3
TarunRaj16 <tarunrajendran@gmail.com>
2014-09-20 14:40:53 -0500

    improved index.html and indexjs.js

---
+++ indexjs.js
@@ -0,0 +1,54 @@
+
+
+function send(){
+
+	 
+	 var oldThemeUnsplit = document.getElementById("oldThemes").value;
+	 var oldThemeArray = oldThemeUnsplit.split(" ");
+	 
+	 var newThemeUnsplit = document.getElementById("newThemes").value;
+	 var newThemeArray = newThemeUnsplit.split(" ");
+	 
+	 document.getElementById("oldThemes").style.backgroundColor='green';
+	 document.getElementById("newThemes").style.backgroundColor='green';
+
+	 sendToServer(JSON.stringify(oldThemeArray),JSON.stringify(newThemeArray));
+	 
+	 alert("Old Themes: " + oldThemeArray + " || " + "New Themes" + newThemeArray);
+	 
+	 
+	 }
+	 
+function sendToServer(oldThemeArrayPar, newThemeArrayPar) {
+
+  if ("WebSocket" in window)
+  {
+     alert("WebSocket is supported by your Browser!");
+     // Let us open a web socket
+     var ws = new WebSocket("ws://localhost:3001/echo");
+     ws.onopen = function()
+     {
+        // Web Socket is connected, send data using send()
+        ws.send(oldThemeArrayPar);
+		ws.send(newThemeArrayPar);
+        alert("Message is sent...");
+     };
+     ws.onmessage = function (evt) 
+     { 
+        var received_msg = evt.data;
+        alert("Message is received...");
+     };
+     ws.onclose = function()
+     { 
+        // websocket is closed.
+        alert("Connection is closed..."); 
+     };
+  }
+  else
+  {
+     // The browser doesn't support WebSocket
+     alert("WebSocket NOT supported by your Browser!");
+  }
+
+}
+
