a9a48a513dcfb4a88b1d9874269a64caf03dc5c2
unknown <kcperki2@illinois.edu>
2014-09-19 22:37:59 -0500

    Keyword.py basic necessities

--- Keyword.py
+++ Keyword.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python
+import nltk
+from nltk.corpus import brown
 
 class keyword():
-    def __init__(properties):
+    def __init__(self, properties, text, target):
+        """Initialize keyword class"""
+        self._corpus = nltk.text.ContextIndex(text.tokens)
         self._properties = properties
+        self._target = target
+    def replace(self):
+        simWords = self._corpus.similar_words(self._target, 100)
+        print (simWords)
+
+def main():
+    textG = nltk.Text(word.lower() for word in nltk.corpus.brown.words())
+    pi = keyword(None, textG, "cat")
+    pi.replace()
+
+if __name__=="__main__":
+    main()
     
