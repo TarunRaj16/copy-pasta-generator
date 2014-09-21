33ee65eebeb89795a4bba2fc1199cd7284914b75
Thomas Liu <thomasliu02@gmail.com>
2014-09-21 00:12:43 -0500

    Added button support

--- indexjs.js
+++ indexjs.js
@@ -1,64 +1,66 @@
-	 
-var ws = new WebSocket("ws://localhost:3001/");
 
+var ws = new WebSocket("ws://localhost:3001/");
 
 function init(){ 
 
-
-	if("WebSocket" in window) {
-            ws.onopen = function (event) {
-					onOpen(event);
-            }
-
-            ws.onmessage = function (message) {
-                onMessage(message);
-		console.log(message.data);
-		var myTarget = document.getElementById("text_div")
-		myTarget.innerHTML = message.data;
-		var textBox = document.getElementById('text_box');
-		textBox.value = message.data
-            }
+    $(".loader").hide();
+    if("WebSocket" in window) {
+        ws.onopen = function (event) {
+	    onOpen(event);
         }
- 
+
+        ws.onmessage = function (message) {
+            onMessage(message);
+	    console.log(message.data);
+	    var myTarget = document.getElementById("text_div");
+	    myTarget.innerHTML = message.data;
+	    var textBox = document.getElementById('text_box');
+	    textBox.value = message.data;
+	    var loading_box = document.getElementById("fancyloader");
+	    $(".loader").hide();
+        }
+    }
+    
 }  
- 
- function onOpen(evt) { 
-	// alert("CONNECTED"); 
- } 
- 
- function onMessage(evt) { 
-	// alert("Receiving Message"); 
- }  
-  
- function sendToServer(message) { 
- 
-	// alert("Sending Message");
-	ws.send(message); 
- 
- }  
- 
+
+function onOpen(evt) { 
+    // alert("CONNECTED"); 
+} 
+
+function onMessage(evt) { 
+    // alert("Receiving Message");
+    $(".loader").hide();
+}  
+
+function sendToServer(message) { 
+    
+    // alert("Sending Message");
+    ws.send(message); 
+    
+}  
+
 
 
 function send(){
+    var loading_box = document.getElementById("fancyloader");
+    $(".loader").show();
+    var oldThemeUnsplit = document.getElementById("oldThemes").value;
+    var oldThemeArray = oldThemeUnsplit.split(" ");
+    
+    var newThemeUnsplit = document.getElementById("newThemes").value;
+    var newThemeArray = newThemeUnsplit.split(" ");
+    
+    document.getElementById("oldThemes").style.backgroundColor='green';
+    document.getElementById("newThemes").style.backgroundColor='green';
 
-	 
-	 var oldThemeUnsplit = document.getElementById("oldThemes").value;
-	 var oldThemeArray = oldThemeUnsplit.split(" ");
-	 
-	 var newThemeUnsplit = document.getElementById("newThemes").value;
-	 var newThemeArray = newThemeUnsplit.split(" ");
-	 
-	 document.getElementById("oldThemes").style.backgroundColor='green';
-	 document.getElementById("newThemes").style.backgroundColor='green';
-
-	 var togetherArray = [oldThemeArray,newThemeArray];
-	 var jsonstring = JSON.stringify(togetherArray);
-	 // alert("JSON STRING: " + jsonstring);
-	 sendToServer(jsonstring);
-	 
-	 
-	 }
+    var togetherArray = [oldThemeArray,newThemeArray];
+    var jsonstring = JSON.stringify(togetherArray);
+    // alert("JSON STRING: " + jsonstring);
+    sendToServer(jsonstring);
+    
+    
+}
 
 
-	 
+
 
